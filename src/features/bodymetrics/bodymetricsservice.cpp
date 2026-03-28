bdfd68919517c468bd8fb364fd62ef78b16f7382
alalayom <alalayom@gmail.com>
2026-03-29 01:27:33 +0300

    Bodymetricsservice added

--- src/features/bodymetrics/bodymetricsservice.cpp
+++ src/features/bodymetrics/bodymetricsservice.cpp
@@ -1,6 +1,326 @@
 #include "bodymetricsservice.h"
 
-BodyMetricsService::BodyMetricsService()
+BodyMetricsService::BodyMetricsService(DatabaseManager *pDatabaseManager, QObject *pParent)
+    : QObject(pParent)
+    , mDatabaseManager(pDatabaseManager)
+    , mRepository(pDatabaseManager)
+    , mMeasurement()
 {
 
+}
+
+const BodyMeasurementEntry &BodyMetricsService::measurementEntry() const noexcept
+{
+    return mMeasurement;
+}
+
+QDate BodyMetricsService::getDate() const noexcept
+{
+    return mMeasurement.getMeasurementDate();
+}
+
+void BodyMetricsService::setDate(const QDate &pDate)
+{
+    if(mMeasurement.getMeasurementDate() == pDate)
+    {
+        return;
+    }
+
+    mMeasurement = mRepository.loadMeasurementEntry(pDate);
+
+    emit dateChanged();
+    emit compositionChanged();
+    emit segmentAnalysisChanged();
+    emit measurementChanged();
+}
+
+const BodyCompositionMetrics &BodyMetricsService::getComposition() const noexcept
+{
+    return mMeasurement.getComposition();
+}
+
+void BodyMetricsService::setComposition(const BodyCompositionMetrics &pComposition)
+{
+    mMeasurement.setComposition(pComposition);
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+const BodySegmentAnalysis &BodyMetricsService::getSegmentAnalysis() const noexcept
+{
+    return mMeasurement.getSegmentAnalysis();
+}
+
+void BodyMetricsService::setSegmentAnalysis(const BodySegmentAnalysis &pSegmentAnalysis)
+{
+    mMeasurement.setSegmentAnalysis(pSegmentAnalysis);
+    saveCurrentMeasurement();
+
+    emit segmentAnalysisChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setWeightKg(double pWeightKg)
+{
+    BodyCompositionMetrics tComposition = mMeasurement.getComposition();
+    tComposition.setWeightKg(pWeightKg);
+    mMeasurement.setComposition(tComposition);
+
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setMuscleMassKg(double pMuscleMassKg)
+{
+    BodyCompositionMetrics tComposition = mMeasurement.getComposition();
+    tComposition.setMuscleMassKg(pMuscleMassKg);
+    mMeasurement.setComposition(tComposition);
+
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setMusclePecentage(double pMusclePercentage)
+{
+    BodyCompositionMetrics tComposition = mMeasurement.getComposition();
+    tComposition.setMusclePercentage(pMusclePercentage);
+    mMeasurement.setComposition(tComposition);
+
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setFatMassKg(double pFatMassKg)
+{
+    BodyCompositionMetrics tComposition = mMeasurement.getComposition();
+    tComposition.setFatMassKg(pFatMassKg);
+    mMeasurement.setComposition(tComposition);
+
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setFatPercentage(double pFatPercentage)
+{
+    BodyCompositionMetrics tComposition = mMeasurement.getComposition();
+    tComposition.setFatPercentage(pFatPercentage);
+    mMeasurement.setComposition(tComposition);
+
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setLeanMassKg(double pLeanMassKg)
+{
+    BodyCompositionMetrics tComposition = mMeasurement.getComposition();
+    tComposition.setLeanMassKg(pLeanMassKg);
+    mMeasurement.setComposition(tComposition);
+
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setBodyWaterMassKg(double pBodyWaterMassKg)
+{
+    BodyCompositionMetrics tComposition = mMeasurement.getComposition();
+    tComposition.setBodyWaterMassKg(pBodyWaterMassKg);
+    mMeasurement.setComposition(tComposition);
+
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setBodyWaterPercentage(double pBodyWaterPercentage)
+{
+    BodyCompositionMetrics tComposition = mMeasurement.getComposition();
+    tComposition.setBodyWaterPercentage(pBodyWaterPercentage);
+    mMeasurement.setComposition(tComposition);
+
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setBmi(double pBmi)
+{
+    BodyCompositionMetrics tComposition = mMeasurement.getComposition();
+    tComposition.setBmi(pBmi);
+    mMeasurement.setComposition(tComposition);
+
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setBmr(double pBmr)
+{
+    BodyCompositionMetrics tComposition = mMeasurement.getComposition();
+    tComposition.setBmr(pBmr);
+    mMeasurement.setComposition(tComposition);
+
+    saveCurrentMeasurement();
+
+    emit compositionChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setTrunkMetrics(const SegmentMetrics &pMetrics)
+{
+    BodySegmentAnalysis tAnalysis = mMeasurement.getSegmentAnalysis();
+    tAnalysis.setTrunk(pMetrics);
+    mMeasurement.setSegmentAnalysis(tAnalysis);
+
+    saveCurrentMeasurement();
+
+    emit segmentAnalysisChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setRightArmMetrics(const SegmentMetrics &pMetrics)
+{
+    BodySegmentAnalysis tAnalysis = mMeasurement.getSegmentAnalysis();
+    tAnalysis.setRightArm(pMetrics);
+    mMeasurement.setSegmentAnalysis(tAnalysis);
+
+    saveCurrentMeasurement();
+
+    emit segmentAnalysisChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setLeftArmMetrics(const SegmentMetrics &pMetrics)
+{
+    BodySegmentAnalysis tAnalysis = mMeasurement.getSegmentAnalysis();
+    tAnalysis.setLeftArm(pMetrics);
+    mMeasurement.setSegmentAnalysis(tAnalysis);
+
+    saveCurrentMeasurement();
+
+    emit segmentAnalysisChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setRightLegMetrics(const SegmentMetrics &pMetrics)
+{
+    BodySegmentAnalysis tAnalysis = mMeasurement.getSegmentAnalysis();
+    tAnalysis.setRightLeg(pMetrics);
+    mMeasurement.setSegmentAnalysis(tAnalysis);
+
+    saveCurrentMeasurement();
+
+    emit segmentAnalysisChanged();
+    emit measurementChanged();
+}
+
+void BodyMetricsService::setLeftLegMetrics(const SegmentMetrics &pMetrics)
+{
+    BodySegmentAnalysis tAnalysis = mMeasurement.getSegmentAnalysis();
+    tAnalysis.setLeftLeg(pMetrics);
+    mMeasurement.setSegmentAnalysis(tAnalysis);
+
+    saveCurrentMeasurement();
+
+    emit segmentAnalysisChanged();
+    emit measurementChanged();
+}
+
+QVariantMap BodyMetricsService::compositionAsVariantMap() const
+{
+    QVariantMap tMap;
+    const auto& tComposition = mMeasurement.getComposition();
+
+    tMap["weightKg"] = tComposition.getWeightKg();
+    tMap["muscleMassKg"] = tComposition.getMuscleMassKg();
+    tMap["musclePercentage"] = tComposition.getMusclePercentage();
+    tMap["fatMassKg"] = tComposition.getFatMassKg();
+    tMap["fatPercentage"] = tComposition.getFatPercentage();
+    tMap["leanMassKg"] = tComposition.getLeanMassKg();
+    tMap["bodyWaterMassKg"] = tComposition.getBodyWaterMassKg();
+    tMap["bodyWaterPercentage"] = tComposition.getBodyWaterPercentage();
+    tMap["bmi"] = tComposition.getBmi();
+    tMap["bmr"] = tComposition.getBmr();
+
+    return tMap;
+}
+
+QVariantMap BodyMetricsService::segmentAnalysisAsVariantMap() const
+{
+    QVariantMap tMap;
+    const auto& tAnalysis = mMeasurement.getSegmentAnalysis();
+
+    tMap["trunk"] = segmentToVariant(tAnalysis.getTrunk());
+    tMap["rightArm"] = segmentToVariant(tAnalysis.getRightArm());
+    tMap["leftArm"] = segmentToVariant(tAnalysis.getLeftArm());
+    tMap["rightLeg"] = segmentToVariant(tAnalysis.getRightLeg());
+    tMap["leftLeg"] = segmentToVariant(tAnalysis.getLeftLeg());
+
+    return tMap;
+}
+
+QVariantMap BodyMetricsService::measurementAsVariantMap() const
+{
+    QVariantMap tMap;
+
+    tMap["date"] = mMeasurement.getMeasurementDate().toString(Qt::ISODate);
+    tMap["composition"] = compositionAsVariantMap();
+    tMap["segmentAnalysis"] = segmentAnalysisAsVariantMap();
+
+    return tMap;
+}
+
+bool BodyMetricsService::saveCurrentMeasurement()
+{
+    return mRepository.saveMeasurementEntry(mMeasurement);
+}
+
+void BodyMetricsService::loadCurrentMeasurement()
+{
+    mMeasurement = mRepository.loadMeasurementEntry(mMeasurement.getMeasurementDate());
+}
+
+bool BodyMetricsService::deleteCurrentMeasurement()
+{
+    const bool tSuccess = mRepository.deleteMeasurementEntry(mMeasurement.getMeasurementDate());
+
+    if(tSuccess)
+    {
+        mMeasurement = BodyMeasurementEntry();
+
+        emit dateChanged();
+        emit compositionChanged();
+        emit segmentAnalysisChanged();
+        emit measurementChanged();
+    }
+
+    return tSuccess;
+}
+
+QVariantMap BodyMetricsService::segmentToVariant(const SegmentMetrics &pMetrics)
+{
+    QVariantMap tMap;
+
+    tMap["muscleMassKg"] = pMetrics.getMuscleMassKg();
+    tMap["fatMassKg"] = pMetrics.getFatMassKg();
+    tMap["fatPercentage"] = pMetrics.getFatPercentage();
+    tMap["leanMassKg"] = pMetrics.getLeanMassKg();
+
+    return tMap;
 }
--- src/features/bodymetrics/bodymetricsservice.h
+++ src/features/bodymetrics/bodymetricsservice.h
@@ -1,10 +1,73 @@
 #ifndef BODYMETRICSSERVICE_H
 #define BODYMETRICSSERVICE_H
 
-class BodyMetricsService
+#include <QObject>
+#include <QDate>
+#include <QVariantMap>
+
+#include "bodymeasuremententry.h"
+#include "bodycompositionmetrics.h"
+#include "bodysegmentanalysis.h"
+#include "segmentmetrics.h"
+#include "bodymetricsrepository.h"
+
+#include "../../core/databasemanager.h"
+
+class BodyMetricsService final : public QObject
 {
+    Q_OBJECT
+
 public:
-    bodymetricsservice();
+    explicit BodyMetricsService(DatabaseManager* pDatabaseManager, QObject* pParent = nullptr);
+
+    const BodyMeasurementEntry& measurementEntry() const noexcept;
+
+    QDate getDate() const noexcept;
+    void setDate(const QDate& pDate);
+
+    const BodyCompositionMetrics& getComposition() const noexcept;
+    void setComposition(const BodyCompositionMetrics& pComposition);
+
+    const BodySegmentAnalysis& getSegmentAnalysis() const noexcept;
+    void setSegmentAnalysis(const BodySegmentAnalysis& pSegmentAnalysis);
+
+    void setWeightKg(double pWeightKg);
+    void setMuscleMassKg(double pMuscleMassKg);
+    void setMusclePecentage(double pMusclePercentage);
+    void setFatMassKg(double pFatMassKg);
+    void setFatPercentage(double pFatPercentage);
+    void setLeanMassKg(double pLeanMassKg);
+    void setBodyWaterMassKg(double pBodyWaterMassKg);
+    void setBodyWaterPercentage(double pBodyWaterPercentage);
+    void setBmi(double pBmi);
+    void setBmr(double pBmr);
+
+    void setTrunkMetrics(const SegmentMetrics& pMetrics);
+    void setRightArmMetrics(const SegmentMetrics& pMetrics);
+    void setLeftArmMetrics(const SegmentMetrics& pMetrics);
+    void setRightLegMetrics(const SegmentMetrics& pMetrics);
+    void setLeftLegMetrics(const SegmentMetrics& pMetrics);
+
+    QVariantMap compositionAsVariantMap() const;
+    QVariantMap segmentAnalysisAsVariantMap() const;
+    QVariantMap measurementAsVariantMap() const;
+
+    bool saveCurrentMeasurement();
+    void loadCurrentMeasurement();
+    bool deleteCurrentMeasurement();
+
+signals:
+    void dateChanged();
+    void compositionChanged();
+    void segmentAnalysisChanged();
+    void measurementChanged();
+
+private:
+    static QVariantMap segmentToVariant(const SegmentMetrics& pMetrics);
+
+    DatabaseManager* mDatabaseManager = nullptr;
+    BodyMetricsRepository mRepository;
+    BodyMeasurementEntry mMeasurement;
 };
 
 #endif // BODYMETRICSSERVICE_H
